1a1734163a6c41f9c16d2cb744a77cadbee0016b
kwon <tnsdyd10@naver.com>
2022-08-15 17:40:28 +0900

    deploy

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const DEBUG: boolean = true;
+export const DEBUG: boolean = false;
 
 export const API_URL: string = "https://api.themoviedb.org/3/movie"
-export const SERVER_URL: string = DEBUG ? "http://localhost:3000" : "";
+export const SERVER_URL: string = DEBUG ? "http://localhost:3000" : "boyfromthewell-next-js.netlify.app";
