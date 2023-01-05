74dd4c45b532c4e04fd2872722cb8bdd189d0c8b
tkdkagody <80307779+tkdkagody@users.noreply.github.com>
2023-01-06 07:31:16 +0900

    fixx..

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: "/stages/:path*",
-        destination: "http://moverse.club/stages/:path*",
+        destination: "http://aa/:path*",
         permanent: false,
       },
     ];
