06b58b3a83455da48f8e0c757beec41231a82c68
tkdkagody <80307779+tkdkagody@users.noreply.github.com>
2023-01-06 07:16:17 +0900

    fix..

--- constants.ts
+++ constants.ts
@@ -3,4 +3,4 @@ export const DEBUG: boolean = false;
 export const API_URL: string = "https://api.themoviedb.org/3/movie";
 export const SERVER_URL: string = DEBUG
   ? "http://localhost:3000"
-  : "https://boyfromthewell-next-js.netlify.app";
+  : "https://imaginative-beijinho-62a328.netlify.app";
