eb97077a4f341d14868b96769ad31156949d58cf
tkdkagody <80307779+tkdkagody@users.noreply.github.com>
2023-01-06 08:14:19 +0900

    fixx..

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -4,7 +4,13 @@ import { SeoProps } from "../types";
 const Seo = ({ title }: SeoProps) => {
   return (
     <Head>
-      <title>{title}</title>
+      <title>MOVERSE.CLUB</title>
+      <meta property="og:title" content={title} />
+      {/* <meta property="og:url" content="https://moverse.club/" /> */}
+      <meta
+        property="og:description"
+        content="3D Motion Solution, Your Move Universe"
+      />
     </Head>
   );
 };
