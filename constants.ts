ba2207c72a4731cfec7389d624e325e2f8299c91
tkdkagody <80307779+tkdkagody@users.noreply.github.com>
2023-01-06 10:16:43 +0900

    remove: Remove constans

--- constants.ts
+++
@@ -1,6 +0,0 @@
-export const DEBUG: boolean = false;
-
-export const API_URL: string = "https://api.themoviedb.org/3/movie";
-export const SERVER_URL: string = DEBUG
-  ? "http://localhost:3000"
-  : `"https://imaginative-beijinho-62a328.netlify.app"`;
